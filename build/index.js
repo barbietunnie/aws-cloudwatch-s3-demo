569994c31b00f76925ca42b13147f46d154bddae
Babatunde Adeyemi <badeyemi@baosystems.com>
2018-10-03 11:55:21 +0100

    Added 2 different compression methods

--- build/index.js
+++ build/index.js
@@ -12,6 +12,14 @@ var _cwlogsWritable = require('cwlogs-writable');
 
 var _cwlogsWritable2 = _interopRequireDefault(_cwlogsWritable);
 
+var _zlib = require('zlib');
+
+var _zlib2 = _interopRequireDefault(_zlib);
+
+var _archiver = require('archiver');
+
+var _archiver2 = _interopRequireDefault(_archiver);
+
 var _config = require('../config.json');
 
 var _config2 = _interopRequireDefault(_config);
@@ -72,4 +80,83 @@ var streamLogs = function streamLogs() {
     });
 };
 
-streamLogs();
+var compressAppData = function compressAppData() {
+    var appDataDir = _appRootPath2.default.resolve('data/storage');
+    var destArchive = _appRootPath2.default.resolve('data/' + Math.round(Math.random() * 4026531839 + 268435456).toString(16) + '.tar.gz'); // change later to a temp dir
+    console.log('Directory: ' + appDataDir);
+    console.log('Destination Archive: ' + destArchive + '\n');
+
+    var outputStream = _fs2.default.createWriteStream(destArchive);
+    var gzip = _zlib2.default.createGzip();
+
+    _fs2.default.readdir(appDataDir, function (err, files) {
+        console.log(files.length + ' file(s) were found in the directory: ');
+        files.forEach(function (file) {
+            console.log('- ' + appDataDir + '/' + file);
+        });
+
+        var ind = 0;
+        Promise.all(files.map(function (file, index) {
+            // Skip hidden files and the authentication file
+            if (!file.endsWith('.json') || file === 'auth.json' || ind > 0) return new Promise(function (resolve) {
+                resolve();
+            });
+
+            console.log(ind + '. ' + file);
+            ind++;
+
+            return new Promise(function (resolve, reject) {
+                console.log('Reading ' + appDataDir + '/' + file + '...');
+                var inputStream = _fs2.default.createReadStream(appDataDir + '/' + file);
+
+                inputStream.pipe(gzip).pipe(outputStream).on('finish', function (err) {
+                    if (err) {
+                        console.error(err);
+                        return reject(err);
+                    }
+
+                    resolve();
+                });
+            });
+        })).then(console.log('Compression completed'));
+    });
+};
+
+var compressAppData2 = function compressAppData2() {
+    var appDataDir = _appRootPath2.default.resolve('data/storage');
+    var destArchive = _appRootPath2.default.resolve('data/' + Math.round(Math.random() * 4026531839 + 268435456).toString(16) + '.zip'); // change later to a temp dir
+    console.log('Directory: ' + appDataDir);
+    console.log('Destination Archive: ' + destArchive + '\n');
+
+    var archive = (0, _archiver2.default)('zip', {
+        gzip: true,
+        zlib: { level: 9 }
+    });
+    var outputStream = _fs2.default.createWriteStream(destArchive);
+    archive.pipe(outputStream);
+
+    _fs2.default.readdir(appDataDir, function (err, files) {
+        console.log(files.length + ' file(s) were found in the directory: ');
+        files.forEach(function (file) {
+            console.log('- ' + appDataDir + '/' + file);
+        });
+
+        Promise.all(files.map(function (file, index) {
+            // Skip hidden files and the authentication file
+            if (!file.endsWith('.json') || file === 'auth.json') return new Promise(function (resolve) {
+                resolve();
+            });
+
+            return new Promise(function (resolve, reject) {
+                archive.file(appDataDir + '/' + file, { name: file });
+                resolve();
+            });
+        })).then(function () {
+            return archive.finalize();
+        });
+    });
+};
+
+// streamLogs();
+// compressAppData();
+compressAppData2();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import appRoot from 'app-root-path';
 import CWLogsWritable from 'cwlogs-writable';
+import zlib from 'zlib';
+import archiver from 'archiver';
 import config from '../config.json';
 
 const dataDir = appRoot.resolve('data/combined.log');
@@ -58,4 +60,88 @@ const streamLogs = () => {
     });
 };
 
-streamLogs();
+const compressAppData = () => {
+    const appDataDir = appRoot.resolve('data/storage');
+    const destArchive = appRoot.resolve('data/' +
+        Math.round(Math.random() * 4026531839 + 268435456).toString(16) + '.tar.gz'); // change later to a temp dir
+    console.log(`Directory: ${appDataDir}`);
+    console.log(`Destination Archive: ${destArchive}\n`);
+
+    const outputStream = fs.createWriteStream(destArchive);
+    const gzip = zlib.createGzip();
+
+    fs.readdir(appDataDir, (err, files) => {
+        console.log(`${files.length} file(s) were found in the directory: `);
+        files.forEach((file) => {
+            console.log(`- ${appDataDir}/${file}`);
+        });
+
+        let ind = 0;
+        Promise.all(files.map((file, index) => {
+            // Skip hidden files and the authentication file
+            if (!file.endsWith('.json') || file === 'auth.json' || ind > 0)
+                return new Promise(resolve => {
+                    resolve();
+                });
+
+            console.log(`${ind}. ${file}`);
+            ind++;
+
+            return new Promise((resolve, reject) => {
+                console.log(`Reading ${appDataDir}/${file}...`);
+                const inputStream = fs.createReadStream(`${appDataDir}/${file}`);
+
+                inputStream.pipe(gzip).pipe(outputStream).on('finish', err => {
+                    if (err) {
+                        console.error(err);
+                        return reject(err);
+                    }
+
+                    resolve();
+                });
+            });
+        })).then(console.log('Compression completed'));
+    });
+};
+
+const compressAppData2 = () => {
+    const appDataDir = appRoot.resolve('data/storage');
+    const destArchive = appRoot.resolve('data/' +
+        Math.round(Math.random() * 4026531839 + 268435456).toString(16) + '.zip'); // change later to a temp dir
+    console.log(`Directory: ${appDataDir}`);
+    console.log(`Destination Archive: ${destArchive}\n`);
+
+    const archive = archiver('zip', {
+        gzip: true,
+        zlib: { level: 9 }
+    });
+    const outputStream = fs.createWriteStream(destArchive);
+    archive.on('error', function(err) {
+        throw err;
+    });
+    archive.pipe(outputStream);
+
+    fs.readdir(appDataDir, (err, files) => {
+        console.log(`${files.length} file(s) were found in the directory: `);
+        files.forEach((file) => {
+            console.log(`- ${appDataDir}/${file}`);
+        });
+
+        Promise.all(files.map((file, index) => {
+            // Skip hidden files and the authentication file
+            if (!file.endsWith('.json') || file === 'auth.json')
+                return new Promise(resolve => {
+                    resolve();
+                });
+
+            return new Promise((resolve, reject) => {
+                archive.file(`${appDataDir}/${file}`, { name: file });
+                resolve();
+            });
+        })).then(() => archive.finalize());
+    });
+};
+
+// streamLogs();
+// compressAppData();
+compressAppData2();
